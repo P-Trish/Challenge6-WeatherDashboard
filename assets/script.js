687892f89970c51849d411c1d7e47814e88f1070
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-03-27 21:55:52 -0700

    added notes for myself to remember tomorrow about geolocation and fetching

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 var date = dayjs();
 $('#cityStats').text(date.format ('ddd, MM/ DD/ YYYY'));
 
+// will eventually need to create a variable for geolocation to pull lat and longitude in api weather fetch 
+// create variable breaking up api URL and calling it back together with fetched data of location coordinates and city 
+
 fetch('https://api.openweathermap.org/data/2.5/forecast?lat=34.1808&lon=-118.3090&appid=d1525879b423538907af5db6aa1d4658')
 .then(function (response){
     return response.json();
@@ -15,7 +18,7 @@ citySearchBtn.addEventListener('click', () => {
     localStorage.setItem('city', cityPlace);
     console.log (cityPlace)
 
-    
+
 // create element to append to page where the citySearch aside gets populated with user inputed cities in their search 
 
 });
