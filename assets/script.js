0609297c564eeff97a7eee0b8ccd3212a9786fbf
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-03-27 21:47:17 -0700

    did some java fetch weather API

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,21 @@
 var date = dayjs();
 $('#cityStats').text(date.format ('ddd, MM/ DD/ YYYY'));
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?lat=34.1808&lon=-118.3090&appid=d1525879b423538907af5db6aa1d4658')
+.then(function (response){
+    return response.json();
+})
+.then(function (data){
+    console.log(data);
+});
+
+citySearchBtn.addEventListener('click', () => {
+    var cityPlace = inputCity.value;
+
+    localStorage.setItem('city', cityPlace);
+    console.log (cityPlace)
+
+    
+// create element to append to page where the citySearch aside gets populated with user inputed cities in their search 
+
+});
