c3df81ecc52fe3c73be4aa87aa97da18cd558cb5
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-03-28 16:56:03 -0700

    added event listener to search button

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 var date = dayjs();
-$('#cityStats').text(date.format ('ddd, MM/ DD/ YYYY'));
+$('#cityStats').text(date.format ('ddd, MM/ DD/ YYYY hh:mm A'));
+
+var inputCity = document.getElementById('inputCity');
+
+var citySearchBtn = document.getElementById('citySearchBtn');
 
 // will eventually need to create a variable for geolocation to pull lat and longitude in api weather fetch 
 // create variable breaking up api URL and calling it back together with fetched data of location coordinates and city 
@@ -18,7 +22,22 @@ citySearchBtn.addEventListener('click', () => {
     localStorage.setItem('city', cityPlace);
     console.log (cityPlace)
 
+    var place = localstorage.getItem ('city');
+    console.log('city');
 
 // create element to append to page where the citySearch aside gets populated with user inputed cities in their search 
 
+// remember this: i created an element here to be appended later 
+var cityInputEl = $('<button>');
+
+
 });
+
+// will need to create & append TEMP, WIND, HUMIDITY + weather indication icon -- from API pull weather: icon, main; pull wind: gust, weather -> main: humidity
+
+var weatherResults = {
+    temp: temp.value,
+    wind: weather.icon ,
+    humidity: weather.humidity,
+  };
+  
